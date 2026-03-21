76e70ef04e6bfbb9bd0841ef9f3141d9e946be5f
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2026-03-21 20:53:58 +0100

    Update/improve better tab source file.

--- src/better-tab.js
+++ src/better-tab.js
@@ -7,11 +7,14 @@ function BetterTab(chromeTab) {
     this.removed = false;
     this.close = this.close.bind(this);
     this.remove = this.close.bind(this);
-    this.detectLanguage = this.detectLanguage.bind(this)
-    this.discard = this.discard.bind(this)
-    this.reload = this.reload.bind(this)
+    this.detectLanguage = this.detectLanguage.bind(this);
+    this.discard = this.discard.bind(this);
+    this.duplicate = this.duplicate.bind(this);
+    this.reload = this.reload.bind(this);
     this.update = this.update.bind(this);
     this.focus = this.focus.bind(this);
+    this.mute = this.mute.bind(this);
+    this.unmute = this.unmute.bind(this);
 };
 
 BetterTab.close = async function() {
@@ -22,19 +25,15 @@ BetterTab.close = async function() {
     // TODO: "Garbage collect" | nullify this, self-destruct ?
 };
 
-BetterTab.discard = async function () {
-    if (this.removed) return
-    const tab = await chrome.tabs.discard(this.id)
-    if (tab) return new BetterTab(tab)
-    return undefined
-}
+BetterTab.discard = function () {
+    if (this.removed) return;
+    return chrome.tabs.discard(this.id);
+};
 
-BetterTab.duplicate = async function() {
-    if (this.removed) return
-    const tab = await chrome.tabs.duplicate(this.id)
-    if (tab) return new BetterTab(tab)
-    return undefined
-}
+BetterTab.duplicate = function () {
+    if (this.removed) return;
+    return chrome.tabs.duplicate(this.id);
+};
 
 BetterTab.mute = async function(callback) {
     return await this.update({muted:true}, callback)
@@ -50,18 +49,12 @@ BetterTab.unmute = async function(callback) {
     return await this.update({muted:false}, callback)
 }
 
-BetterTab.update = function(payload, callback) {
+BetterTab.update = function (payload, callback) {
     if (!this.removed) {
         if (callback) {
-            chrome.tabs.update(
-                this.id, payload, (tab) => callback(new BetterTab(tab))
-            );
+            chrome.tabs.update(this.id, payload, callback);
         } else {
-            return new Promise(resolve => {
-                chrome.tabs.update(this.id, payload, tab => {
-                    resolve(new BetterTab(tab))
-                });
-            });
+            return chrome.tabs.update(this.id, payload);
         }
     }
 };
@@ -71,19 +64,9 @@ BetterTab.detectLanguage = async function() {
     return await chrome.tabs.detectLanguage(this.id)
 }
 
-BetterTab.focus = function(callback) {
+BetterTab.focus = function (callback) {
     if (!this.removed) {
-        if (callback) {
-            this.update(
-                {active:true}, (tab) => callback(new BetterTab(tab))
-            );
-        } else {
-            return new Promise(resolve => {
-                this.update({active:true}, tab => {
-                    resolve(new BetterTab(tab))
-                });
-            });
-        }
+        return this.update({ active: true }, callback);
     }
 };
 
@@ -175,9 +158,97 @@ const gcp = new Proxy(chrome.tabs.getCurrent, {
     }
 })
 
+const up = new Proxy(chrome.tabs.update, {
+    apply(fn, _, args) {
+        const lastArg = args[args.length - 1];
+        const cb = typeof lastArg === "function" ? lastArg : null;
+        if (cb) {
+            args[args.length - 1] = (tab) => cb(new BetterTab(tab));
+            fn.apply(null, args);
+        } else {
+            return new Promise((res, rej) => {
+                fn(...args, (tab) => {
+                    if (chrome.runtime && chrome.runtime.lastError) {
+                        rej(chrome.runtime.lastError);
+                    } else {
+                        res(new BetterTab(tab));
+                    }
+                });
+            });
+        }
+    }
+});
+
+const dp = new Proxy(chrome.tabs.duplicate, {
+    apply(fn, _, args) {
+        const cb = args[1];
+        if (cb) {
+            fn(args[0], (tab) => cb(tab ? new BetterTab(tab) : undefined));
+        } else {
+            return new Promise((res, rej) => {
+                fn(...args, (tab) => {
+                    if (chrome.runtime && chrome.runtime.lastError) {
+                        rej(chrome.runtime.lastError);
+                    } else {
+                        res(tab ? new BetterTab(tab) : undefined);
+                    }
+                });
+            });
+        }
+    }
+});
+
+const disp = new Proxy(chrome.tabs.discard, {
+    apply(fn, _, args) {
+        const lastArg = args[args.length - 1];
+        const cb = typeof lastArg === "function" ? lastArg : null;
+        if (cb) {
+            args[args.length - 1] = (tab) => cb(tab ? new BetterTab(tab) : undefined);
+            fn.apply(null, args);
+        } else {
+            return new Promise((res, rej) => {
+                fn(...args, (tab) => {
+                    if (chrome.runtime && chrome.runtime.lastError) {
+                        rej(chrome.runtime.lastError);
+                    } else {
+                        res(tab ? new BetterTab(tab) : undefined);
+                    }
+                });
+            });
+        }
+    }
+});
+
+const mp = new Proxy(chrome.tabs.move, {
+    apply(fn, _, args) {
+        const wrapResult = (result) =>
+            Array.isArray(result)
+                ? result.map((t) => new BetterTab(t))
+                : new BetterTab(result);
+        const cb = args[2];
+        if (cb) {
+            fn(args[0], args[1], (result) => cb(wrapResult(result)));
+        } else {
+            return new Promise((res, rej) => {
+                fn(...args, (result) => {
+                    if (chrome.runtime && chrome.runtime.lastError) {
+                        rej(chrome.runtime.lastError);
+                    } else {
+                        res(wrapResult(result));
+                    }
+                });
+            });
+        }
+    }
+});
+
 chrome.tabs.create = cp;
+chrome.tabs.discard = disp;
+chrome.tabs.duplicate = dp;
 chrome.tabs.get = gp;
-chrome.tabs.query = qp;
 chrome.tabs.getCurrent = gcp;
+chrome.tabs.move = mp;
+chrome.tabs.query = qp;
+chrome.tabs.update = up;
 
 console.debug("Better Tab initalized!")
