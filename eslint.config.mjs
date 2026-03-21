11954fecc28a42b1dcfecac6defaacf1d7692f39
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2026-03-21 20:52:40 +0100

    Add esling configuration.

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+
+export default [
+    {
+        files: ["src/**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "script",
+            globals: {
+                ...globals.browser,
+                chrome: "readonly",
+            },
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "error",
+        },
+    },
+];
